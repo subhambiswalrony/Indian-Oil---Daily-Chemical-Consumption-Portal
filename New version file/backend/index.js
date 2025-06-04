23a816d77ef0fb1c7df9cd207a5987aa1de2943d
Subham Biswal <biswalsubhamrony@gmail.com>
2025-06-04 09:42:25 +0530

    backend updated

--- New version file/backend/index.js
+++ New version file/backend/index.js
@@ -1,7 +1,6 @@
-// backend/index.js
 const express = require('express');
 const mysql = require('mysql');
-const mysql = require('mysql2');
+// const mysql = require('mysql2');
 const cors = require('cors');
 
 const app = express();
