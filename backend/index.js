6f05a647919a1c07f25f86495438c9299980813d
Subham Biswal <biswalsubhamrony@gmail.com>
2025-06-20 21:03:12 +0530

    .env

--- backend/index.js
+++ backend/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mysql = require('mysql');
 const cors = require('cors');
 const bcrypt = require('bcrypt');
+require('dotenv').config();
+
 
 const app = express();
 app.use(cors({
@@ -12,10 +14,11 @@ app.use(cors({
 app.use(express.json());
 
 const db = mysql.createConnection({
-  host: 'localhost',
-  user: 'root',
-  password: 'Rony0911',   // Set your MySQL root password here
-  database: 'iocl_chemical_form'
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
 });
 
 db.connect((err) => {
