d5b59ce071bc0b11ca7ca2e942c9a839d1bf903a
Subham Biswal <biswalsubhamrony@gmail.com>
2025-06-22 21:50:39 +0530

    users

--- Database/users.sql
+++ Database/users.sql
@@ -2,7 +2,7 @@ CREATE TABLE users (
     id SERIAL PRIMARY KEY,
     first_name VARCHAR(100),
     last_name VARCHAR(100),
-    email VARCHAR(100),
+    email VARCHAR(100) UNIQUE,
     phone VARCHAR(20),
     password VARCHAR(255),
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
