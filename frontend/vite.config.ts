46c77659dbec1b841c877939b3a1882f9d2524bb
Subham Biswal <biswalsubhamrony@gmail.com>
2025-06-26 23:25:51 +0530

    vite_config

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,23 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'public/_redirects',
+          dest: '.', // copy to `dist/` root
+        },
+      ],
+    }),
+  ],
   server: {
     host: true,
-    port: 5173
+    port: 5173,
   },
   optimizeDeps: {
     exclude: ['lucide-react'],
